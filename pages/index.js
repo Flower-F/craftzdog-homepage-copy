99ab44354796b5d11d9f12c9daafc2b60cfe551c
FlowerF <1647231392@qq.com>
2022-05-30 15:29:21 +0800

    feat: 添加个人头像

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import { Box, Container, Heading } from '@chakra-ui/react';
+import { Box, Container, Heading, Image, useColorModeValue } from '@chakra-ui/react';
 
 const Home = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg="red" align="center" p={3} mb={6}>
+      <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} align="center" p={3} mb={6}>
         Hello, I&apos;m a frontend hobbyist from China!
       </Box>
 
@@ -13,6 +13,18 @@ const Home = () => {
             Flower F
           </Heading>
           <p>A Front End Developer</p>
+        </Box>
+        <Box flexShrink={0} mt={{ base: 4, md: 0 }} ml={{ md: 6 }} align="center">
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth={100}
+            display="inline-block"
+            borderRadius="full"
+            src="/images/flower-f.jpg"
+            alt="Profile Image"
+          />
         </Box>
       </Box>
     </Container>
