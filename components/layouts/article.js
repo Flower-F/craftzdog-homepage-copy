35ea8b2eaad123292da970fc016d4613e12ac028
FlowerF <1647231392@qq.com>
2022-05-31 09:26:54 +0800

    feat: 完成inkdrop page

--- components/layouts/article.js
+++ components/layouts/article.js
@@ -20,7 +20,7 @@ const Layout = ({ children, title }) => (
     <>
       {title && (
         <Head>
-          <title>{title} = Flower F</title>
+          <title>{title} - Flower F</title>
         </Head>
       )}
       {children}
